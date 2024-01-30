f868bbcdf628662f30634dd1802b4b16b9a48270
carloscabe <155786810+carloscabe@users.noreply.github.com>
2024-01-29 21:44:42 -0400

    Add files via upload

---
+++ frutas y preparados.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>//cadena
+using namespace std;
+
+string frutas[]={"manzana", "banana", "naranja", "fresa", "kiwi", "mango", "pera", "melon", "papaya", "uva"};
+string preparados[]={"jugo", "ensalada", "frape", "tarta", "helado", "mermelada", "batido", "asado", "coctel", "crudo"};
+string fruta[5];
+string preparado[5];
+
+string generarFruta()
+{
+	return frutas[rand()%10];
+}	
+string generarPreparado()
+{
+	return preparados[rand()%10];
+}
+void generar(){
+	for(int i=0;i<5;i++)
+	{
+		fruta[i]=generarFruta();
+		preparado[i]=generarPreparado();
+	}
+}
+void imprimir()
+{
+	cout<<"menu de productos"<<endl;
+	for(int i=0;i<5;i++)
+		cout<<preparado[i]<<" de "<<fruta[i]<<endl;
+}
+	
+int main(int argc, char *argv[]) {
+	generar();
+	imprimir();
+	return 0;
+}
+	
+
